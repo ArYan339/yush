8d4f9417140b30441f6d19af21eb1471c7bcb6dd
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2025-05-31 07:59:02 -0700

    Add files via upload

---
+++ src/components/icons/types.ts
@@ -0,0 +1,20 @@
+export interface IconPath {
+  path: string;
+  viewBox?: string;
+}
+
+export type IconName = 
+  | 'arrow-left'
+  | 'arrow-right'
+  | 'github-logo'
+  | 'instagram-logo'
+  | 'linkedin-logo'
+  | 'list'
+  | 'microphone-stage'
+  | 'moon-stars'
+  | 'paper-plane-tilt'
+  | 'rocket-launch'
+  | 'sun'
+  | 'video-editing'
+  | 'davinci-resolve'
+  | 'youtube-logo';
